5e6e6ae4e2c7d77e5e464c089d42ac4a06d5ca65
Polarified <polarified@gmail.com>
2020-07-14 19:54:31 +0300

    app client and server

---
+++ api-client.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import socket
+import selectors
+import traceback
+
+import libclient
+
+HOST = "10.0.0.11"
+PORT = 65432
+
+
+def create_request(action, value):
+    if action == "search":
+        return dict(
+            type="text/json",
+            encoding="utf-8",
+            content=dict(action=action, value=value),
+        )
+    else:
+        return dict(
+            type="binary/custom-client-binary-type",
+            encoding="binary",
+            content=bytes(action + value, encoding="utf-8"),
+        )
+
+
+def start_connection(host, port, request, sel):
+    addr = (host, port)
+    print("starting connection to", addr)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex(addr)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    message = libclient.Message(sel, sock, addr, request)
+    sel.register(sock, events, data=message)
+
+
+def main():
+    sel = selectors.DefaultSelector()
+
+    action = input("Enter action: ")
+    value = input("Enter value: ")
+
+    request = create_request(action, value)
+    start_connection(HOST, PORT, request, sel)
+
+    try:
+        while True:
+            events = sel.select(timeout=1)
+            for key, mask in events:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        "main: error: exception for",
+                        f"{message.addr}:\n{traceback.format_exc()}",
+                    )
+                    message.close()
+            # Check for a socket being monitored to continue.
+            if not sel.get_map():
+                break
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ api-server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import socket
+import selectors
+import traceback
+
+import libserver
+
+HOST = "10.0.0.11"
+PORT = 65432
+
+
+def accept_wrapper(sock, sel):
+    conn, addr = sock.accept()  # Should be ready to read
+    print("accepted connection from", addr)
+    conn.setblocking(False)
+    message = libserver.Message(sel, conn, addr)
+    sel.register(conn, selectors.EVENT_READ, data=message)
+
+
+def main():
+    sel = selectors.DefaultSelector()
+
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Avoid bind() exception: OSError: [Errno 48] Address already in use
+    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    lsock.bind((HOST, PORT))
+    lsock.listen()
+    print("listening on", (HOST, PORT))
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
+    try:
+        while True:
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj, sel)
+                else:
+                    message = key.data
+                    try:
+                        message.process_events(mask)
+                    except Exception:
+                        print(
+                            "main: error: exception for",
+                            f"{message.addr}:\n{traceback.format_exc()}",
+                        )
+                        message.close()
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
+if __name__ == '__main__':
+    main()
